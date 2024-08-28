08b2e4a428bb91d0d4e85784be4d927a30517ea8
Asisak08 <tizaaa08@gmail.com>
2024-08-28 10:59:23 +0700

    start

---
+++ login/connectdata.php
@@ -0,0 +1,22 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=mydb";
+$username ="root";
+$password="";
+
+/* 
+try{
+    $obj = new PDO($dns,$username,$password);
+    echo "เชื่อมต่อฐานข้อมูลสำเร็จ"; 
+
+}catch(PDOException $e){
+    echo $e->getMessage();
+} 
+*/
+
+
+
+
+
+
+?>
